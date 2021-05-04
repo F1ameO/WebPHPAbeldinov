1f347a8dcb48b8322734433cc7e99b715d08b69a
F1ameO <73456223+F1ameO@users.noreply.github.com>
2021-05-04 14:40:13 +0600

    mysite2

---
+++ mysite2.local/inc/cookie.inc.php
@@ -0,0 +1,15 @@
+<?php
+$visitCounter = (isset ($_COOKIE["visitCounter"])) ? $_COOKIE["visitCounter"] : 0;
+$visitCounter++;
+setcookie("visitCounter", $visitCounter, time()+4);
+echo "Пользователь посещал страницу $visitCounter раз";
+
+echo "</br>";
+
+$counter = $_COOKIE["counter"]; 
+if (!isSet($counter)) 
+$counter = date('Y-m-d H:i:s'); 
+else 
+$counter = $counter; 
+echo "Последний раз вы заходили $counter";
+?>
